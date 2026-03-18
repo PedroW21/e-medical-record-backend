c4023353038fde96ec3da3fee7aff4c82ab370dc
PedroW21 <pedrow21br@gmail.com>
2026-03-17 22:31:06 -0300

    feat(medical-record): add Prontuario relationships for exam result types

--- app/Modules/MedicalRecord/Models/Prontuario.php
+++ app/Modules/MedicalRecord/Models/Prontuario.php
@@ -28,6 +28,20 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property-read Prontuario|null $prontuarioBase
  * @property-read \Illuminate\Database\Eloquent\Collection<int, Prescricao> $prescricoes
  * @property-read \Illuminate\Database\Eloquent\Collection<int, ValorLaboratorial> $valoresLaboratoriais
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoEcg> $resultadosEcg
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoRx> $resultadosRx
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoTextoLivre> $resultadosTextoLivre
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, RegistroTemperatura> $registrosTemperatura
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoElastografiaHepatica> $resultadosElastografiaHepatica
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoMapa> $resultadosMapa
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoDexa> $resultadosDexa
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoTesteErgometrico> $resultadosTesteErgometrico
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoEcodopplerCarotidas> $resultadosEcodopplerCarotidas
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoEcocardiograma> $resultadosEcocardiograma
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoMrpa> $resultadosMrpa
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoCat> $resultadosCat
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoCintilografia> $resultadosCintilografia
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, ResultadoPeDiabetico> $resultadosPeDiabetico
  */
 class Prontuario extends Model
 {
@@ -99,6 +113,118 @@ class Prontuario extends Model
     public function valoresLaboratoriais(): HasMany
     {
         return $this->hasMany(ValorLaboratorial::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoEcg, $this>
+     */
+    public function resultadosEcg(): HasMany
+    {
+        return $this->hasMany(ResultadoEcg::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoRx, $this>
+     */
+    public function resultadosRx(): HasMany
+    {
+        return $this->hasMany(ResultadoRx::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoTextoLivre, $this>
+     */
+    public function resultadosTextoLivre(): HasMany
+    {
+        return $this->hasMany(ResultadoTextoLivre::class);
+    }
+
+    /**
+     * @return HasMany<RegistroTemperatura, $this>
+     */
+    public function registrosTemperatura(): HasMany
+    {
+        return $this->hasMany(RegistroTemperatura::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoElastografiaHepatica, $this>
+     */
+    public function resultadosElastografiaHepatica(): HasMany
+    {
+        return $this->hasMany(ResultadoElastografiaHepatica::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoMapa, $this>
+     */
+    public function resultadosMapa(): HasMany
+    {
+        return $this->hasMany(ResultadoMapa::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoDexa, $this>
+     */
+    public function resultadosDexa(): HasMany
+    {
+        return $this->hasMany(ResultadoDexa::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoTesteErgometrico, $this>
+     */
+    public function resultadosTesteErgometrico(): HasMany
+    {
+        return $this->hasMany(ResultadoTesteErgometrico::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoEcodopplerCarotidas, $this>
+     */
+    public function resultadosEcodopplerCarotidas(): HasMany
+    {
+        return $this->hasMany(ResultadoEcodopplerCarotidas::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoEcocardiograma, $this>
+     */
+    public function resultadosEcocardiograma(): HasMany
+    {
+        return $this->hasMany(ResultadoEcocardiograma::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoMrpa, $this>
+     */
+    public function resultadosMrpa(): HasMany
+    {
+        return $this->hasMany(ResultadoMrpa::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoCat, $this>
+     */
+    public function resultadosCat(): HasMany
+    {
+        return $this->hasMany(ResultadoCat::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoCintilografia, $this>
+     */
+    public function resultadosCintilografia(): HasMany
+    {
+        return $this->hasMany(ResultadoCintilografia::class);
+    }
+
+    /**
+     * @return HasMany<ResultadoPeDiabetico, $this>
+     */
+    public function resultadosPeDiabetico(): HasMany
+    {
+        return $this->hasMany(ResultadoPeDiabetico::class);
     }
 
     protected static function newFactory(): \App\Modules\MedicalRecord\Database\Factories\MedicalRecordFactory
